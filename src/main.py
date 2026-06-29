46e23c7f34d9d585c3d77deaa77f37a60e06c083
Guilherme R.C. <pl4ces.h0lder@gmail.com>
2026-06-28 23:35:23 -0300

    Updated main.py

--- src/main.py
+++ src/main.py
@@ -11,27 +11,34 @@ def query_media_gols():
     client = get_bigquery_client()
     query = "SELECT media_gols_brasil FROM `projeto-copa-500721.copa.vw_media-gols-brasil` LIMIT 1"
     df = client.query(query).to_dataframe()
-    return df["media_gols_brasil"].iloc[0] if not df.empty else 0.0
+    if df.empty:
+        return 0.0
+    return df[df.columns[0]].iloc[0]
 
 @st.cache_data(ttl=600)
 def query_porcentagem_nao_perdido():
     client = get_bigquery_client()
     query = "SELECT porcentagem_nao_perdido FROM `projeto-copa-500721.copa.vw_porcentagem-vitorias-empates-brasil` LIMIT 1"
-    return df["porcentagem_nao_perdido"].iloc[0] if not df.empty else 0.0
+    df = client.query(query).to_dataframe()
+    if df.empty:
+        return 0.0
+    return df[df.columns[0]].iloc[0]
 
 @st.cache_data(ttl=600)
 def query_partidas_disputadas():
     client = get_bigquery_client()
     query = "SELECT numero_partidas FROM `projeto-copa-500721.copa.partidas-disputadas-brasil` LIMIT 1"
     df = client.query(query).to_dataframe()
-    return df["numero_partidas"].iloc[0] if not df.empty else 0
+    if df.empty:
+        return 0
+    return df[df.columns[0]].iloc[0]
 
 media_gols_valor = query_media_gols()
 porcentagem_nao_perdido_valor = query_porcentagem_nao_perdido()
 partidas_disputadas_valor = query_partidas_disputadas()
 
 media_gols_formatado = f"{media_gols_valor:.2f}"
-porcentagem_nao_perdido_formatado = f"{porcentagem_nao_perdido_valor}%"
+porcentagem_nao_perdido_formatado = f"{porcentagem_nao_perdido_valor}%" 
 partidas_disputadas_formatado = f"{partidas_disputadas_valor}"
 
 # Interface
