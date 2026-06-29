b436f76c1d7cfd394b005fa9d8c81a1d38f141fd
Guilherme R.C. <pl4ces.h0lder@gmail.com>
2026-06-28 23:24:44 -0300

    Add data from views

--- src/main.py
+++ src/main.py
@@ -7,24 +7,42 @@ def get_bigquery_client():
     return bigquery.Client.from_service_account_info(credentials_info)
 
 @st.cache_data(ttl=600)
-def load_data_from_view():
+def query_media_gols():
     client = get_bigquery_client()
-    
-    query = """
-        SELECT * FROM `seu-projeto-gcp.seu_dataset.sua_view` 
-        LIMIT 100
-    """
-    
-    query_job = client.query(query)
-    df = query_job.to_dataframe()
-    return df
+    query = "SELECT media_gols_brasil FROM `projeto-copa-500721.copa.vw_media-gols-brasil` LIMIT 1"
+    df = client.query(query).to_dataframe()
+    return df["media_gols_brasil"].iloc[0] if not df.empty else 0.0
+
+@st.cache_data(ttl=600)
+def query_porcentagem_nao_perdido():
+    client = get_bigquery_client()
+    query = "SELECT porcentagem_nao_perdido FROM `projeto-copa-500721.copa.vw_porcentagem-vitorias-empates-brasil` LIMIT 1"
+    return df["porcentagem_nao_perdido"].iloc[0] if not df.empty else 0.0
+
+@st.cache_data(ttl=600)
+def query_partidas_disputadas():
+    client = get_bigquery_client()
+    query = "SELECT numero_partidas FROM `projeto-copa-500721.copa.partidas-disputadas-brasil` LIMIT 1"
+    df = client.query(query).to_dataframe()
+    return df["numero_partidas"].iloc[0] if not df.empty else 0
+
+media_gols_valor = query_media_gols()
+porcentagem_nao_perdido_valor = query_porcentagem_nao_perdido()
+partidas_disputadas_valor = query_partidas_disputadas()
+
+media_gols_formatado = f"{media_gols_valor:.2f}"
+porcentagem_nao_perdido_formatado = f"{porcentagem_nao_perdido_valor}%"
+partidas_disputadas_formatado = f"{partidas_disputadas_valor}"
 
 # Interface
 
 st.header("Indicadores") 
 
-col1, col2, col3 = st.columns(3) 
+col1, col2, col3 = st.columns(3)
 
-with col1: st.metric(label="Média de gols", value="00") 
-with col2: st.metric(label="% de jogos sem derrotas", value="00") 
-with col3: st.metric(label="Partidas disputadas", value="00")
+with col1: 
+    st.metric(label="Média de gols", value=media_gols_formatado) 
+with col2: 
+    st.metric(label="% de jogos sem derrotas", value=porcentagem_nao_perdido_formatado) 
+with col3: 
+    st.metric(label="Partidas disputadas", value=partidas_disputadas_formatado)
