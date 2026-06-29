cd72ce0884f6cec508b7e17f337f8657a63bd102
Guilherme R.C. <pl4ces.h0lder@gmail.com>
2026-06-28 23:10:07 -0300

    Add bigquery integration

--- src/main.py
+++ src/main.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from google.cloud import bigquery
+import pandas as pd
+
+def get_bigquery_client():
+    credentials_info = st.secrets["gcp_service_account"]
+    return bigquery.Client.from_service_account_info(credentials_info)
+
+@st.cache_data(ttl=600)
+def load_data_from_view():
+    client = get_bigquery_client()
+    
+    query = """
+        SELECT * FROM `seu-projeto-gcp.seu_dataset.sua_view` 
+        LIMIT 100
+    """
+    
+    query_job = client.query(query)
+    df = query_job.to_dataframe()
+    return df
+
+# Interface
 
 st.header("Indicadores") 
 
