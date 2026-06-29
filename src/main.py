085f93140f2e7e0eff49c437c77ce105d0e09a1d
Guilherme R.C. <pl4ces.h0lder@gmail.com>
2026-06-28 23:46:51 -0300

    Add plot

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import streamlit as st
 from google.cloud import bigquery
 import pandas as pd
+import plotly.express as px
 
 def get_bigquery_client():
     credentials_info = st.secrets["gcp_service_account"]
     return bigquery.Client.from_service_account_info(credentials_info)
+
+# Queries para o Google Cloud
 
 @st.cache_data(ttl=600)
 def query_media_gols():
@@ -33,9 +36,22 @@ def query_partidas_disputadas():
         return 0
     return df[df.columns[0]].iloc[0]
 
+@st.cache_data(ttl=600)
+def query_gols_por_time():
+    client = get_bigquery_client()
+    # Uma query de exemplo que traz o ano e a quantidade de gols
+    query = """
+        SELECT *
+        FROM `projeto-copa-500721.copa.vw_gols-por-time`
+    """
+    df = client.query(query).to_dataframe()
+    return df
+
 media_gols_valor = query_media_gols()
 porcentagem_nao_perdido_valor = query_porcentagem_nao_perdido()
 partidas_disputadas_valor = query_partidas_disputadas()
+
+gols_por_time_grafico = query_gols_por_time()
 
 media_gols_formatado = f"{media_gols_valor:.2f}"
 porcentagem_nao_perdido_formatado = f"{porcentagem_nao_perdido_valor}%" 
@@ -43,7 +59,7 @@ partidas_disputadas_formatado = f"{partidas_disputadas_valor}"
 
 # Interface
 
-st.header("Indicadores") 
+st.header("Indicadores do Brasil") 
 
 col1, col2, col3 = st.columns(3)
 
@@ -53,3 +69,8 @@ with col2:
     st.metric(label="% de jogos sem derrotas", value=porcentagem_nao_perdido_formatado) 
 with col3: 
     st.metric(label="Partidas disputadas", value=partidas_disputadas_formatado)
+
+
+fig = px.bar(gols_por_time_grafico, x="player_team_name", y="gols", title="Gols por time", text_auto=True)
+fig.update_traces(marker_color='green')
+st.plotly_chart(fig, use_container_width=True)
